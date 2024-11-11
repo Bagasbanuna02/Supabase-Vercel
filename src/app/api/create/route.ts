b3c9cfa20e956d106fea2e3d0b41aaedcb09161f
Bagasbanuna02 <bagasbanuna02@gmail.com>
2024-11-11 08:45:11 +0800

    test

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,19 +1,20 @@
-import prisma from "@/app/lib/prisma";
+// import prisma from "@/app/lib/prisma";
 
 export async function POST(req: Request) {
   const data = await req.json();
+  console.log(data);
 
-  const create = await prisma.product.create({
-    data: {
-      name: data.name,
-      price: data.price,
-      typeOfProductId: data.typeId,
-    },
-  });
+  // const create = await prisma.product.create({
+  //   data: {
+  //     name: data.name,
+  //     price: data.price,
+  //     typeOfProductId: data.typeId,
+  //   },
+  // });
 
-  if (!create) {
-    return new Response(JSON.stringify({ error: "Error" }));
-  }
+  // if (!create) {
+  //   return new Response(JSON.stringify({ error: "Error" }));
+  // }
 
   return new Response(JSON.stringify({ status: "success" }));
 }
