ca2cf39a5f31dd2d404355c094e9236f1db2c429
Bagasbanuna02 <bagasbanuna02@gmail.com>
2024-11-17 16:16:04 +0800

    test push main v.1

--- src/app/api/get/product/route.ts
+++ src/app/api/get/product/route.ts
@@ -1,9 +1,11 @@
 import prisma from "@/app/lib/prisma";
 
-export async function GET() {
-  const data = await prisma.product.findMany({
-    orderBy: {
-      createdAt: "desc",
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
+  const data = await prisma.product.findFirst({
+    where: {
+      id: id as string,
     },
   });
   return new Response(JSON.stringify(data));
